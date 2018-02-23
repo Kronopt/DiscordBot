cc8d005c4971d0877d0835d8d0b6c8a069a1b02c
Kronopt <Kronopt@users.noreply.github.com>
2018-02-23 19:16:09 +0000

    !poll stuff
    - A little variable refractoring
    - Verify if options are unique

--- DiscordBot/Commands.py
+++ DiscordBot/Commands.py
@@ -24,7 +24,7 @@ ERROR_MESSAGES = {
     'ping': 'takes no arguments.',
     'hi': 'takes no arguments.',
     'dice': 'takes one of the following arguments: ' + ', '.join(Converters.DICES) + '.',
-    'random': 'either takes no arguments or one of the predefined ones (type `!help random`).',
+    'random': 'either takes no arguments or one of the predefined ones (`!help random` for more information).',
     'between': 'takes 2 integers as arguments.',
     'from': 'takes at least 1 argument.',
     'sum': 'takes at least 1 number.',
@@ -33,8 +33,9 @@ ERROR_MESSAGES = {
     'zero_division_error': 'can\'t divide by zero.',
     'multiply': 'takes at least 1 number.',
     '8ball': 'needs a phrase on which to apply its fortune-telling powers.',
-    'poll': 'takes a poll name as first argument and at least 2 options (type `!help poll`).',
+    'poll': 'takes a poll name as first argument and at least 2 options (`!help poll` for more information).',
     'poll_already_exists': 'A poll is already ongoing with the same name having the following options: ',
+    'poll_non_unique_options': 'Poll options must be different from each other.',
     'vote': 'takes 1 existing poll name and 1 existing option as arguments.',
     'status': 'takes either no arguments or 1 existing poll name as argument.'
 }
@@ -60,8 +61,8 @@ class Commands:
                           "Cannot predict now", "Concentrate and ask again", "Don't count on it",
                           "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-    _polls = {}  # {channel_id: {poll_name: {option: set(user_name)}}}
-    _poll_creator = {}  # {channel_id: {poll_name: author.id}}
+    _polls = {}  # {channel_id: {poll_name: {option: set(user_name)}}}  # TODO maybe create a class to handle this...
+    _poll_creator = {}  # {channel_id: {poll_name: author.id}}  # TODO also class to handle this, maybe...
 
     @staticmethod
     def log_command_call(command):
@@ -205,31 +206,34 @@ class Commands:
 
     # POLL
     @commands.group(name='poll', ignore_extra=False, pass_context=True, invoke_without_command=True)
-    async def command_poll(self, context, poll_name: str, *args: str):
+    async def command_poll(self, context, poll_name: str, *options: str):
         """Creates a poll with the arguments passed as vote options.
-        The first argument is the name of the poll, which is used when voting.
+        The first argument is the name of the poll.
         It can be a single word or any number of space-separated words if enclosed within quotation marks.
         ex:
             poll
             poll_name
             "Is this the poll name?"
 
-        A poll ends when the subcommand 'end' is passed with the poll's name.
+        Each vote option must be unique.
+        A poll ends when the subcommand 'end' is passed with the poll's name as argument.
         Within 5 minutes of the poll's creation only the original author can close the poll."""
-        if len(args) < 2:    # at least two poll options
+        if len(options) < 2:    # at least two poll options
             raise commands.MissingRequiredArgument
         self.log_command_call('poll')
 
         channel = context.message.channel.id
         if channel not in self._polls:
             self._polls[channel] = {}
-        if poll_name in self._polls[channel]:
+        if poll_name in self._polls[channel]:  # Polls must have unique names
             await self.bot.say(ERROR_MESSAGES['poll_already_exists'] +
                                '`' + '`, `'.join(self._polls[channel][poll_name]) + '`')
+        elif len(options) != len(set(options)):  # Options must be different from each other
+            await self.bot.say(ERROR_MESSAGES['poll_non_unique_options'])
         else:
             self._polls[channel][poll_name] = {}
-            for option in args:  # Set all poll options at zero votes
-                self._polls[channel][poll_name][option] = set()
+            for option in options:  # Set all poll options at zero votes
+                self._polls[channel][poll_name][option] = set()  # TODO id for each option (for voting)
 
             if hasattr(context.message.author, 'nick') and context.message.author.nick is not None:
                 author_name = context.message.author.nick
@@ -237,8 +241,8 @@ class Commands:
                 author_name = context.message.author.name
 
             await self.bot.say(author_name + ' created poll `' + poll_name + '` with ' +
-                               str(len(args)) + ' options: `' + '`, `'.join(args) + '`')
-            await self.bot.say('Say `!poll end <poll_name>` to end poll')
+                               str(len(options)) + ' options: `' + '`, `'.join(options) + '`')
+            await self.bot.say('Say `!poll end <poll_name>` to end poll')  # TODO more interesting text
 
             self._poll_creator[channel][poll_name] = context.message.author.id
             await asyncio.sleep(10*60)  # 10 minutes timer
