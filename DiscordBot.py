cffe89dd4902aa5f20c137df27babe2bc8bedb90
Kronopt <Kronopt@users.noreply.github.com>
2020-10-25 17:56:52 +0000

    refactor: Bot class
    All bot-related logic is in this class

--- DiscordBot.py
+++ DiscordBot.py
@@ -3,86 +3,29 @@
 
 
 """
-Bot main .py file.
-A small command line utility is available so that the bot's token is not hardcoded in the script.
+Bot main .py file
+A small command line utility is available so that the bot's token is not hardcoded in the script
 """
 
 
 import argparse
 import logging
-import traceback
-import sys
-import beckett.exceptions
-import discord
-import DiscordBot.Cogs
-from discord.ext import commands
-from DiscordBot.Cogs.Math import Math
-
-
-logging.basicConfig(level=logging.INFO)
-
-command_prefix = '!'
-BOT = commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix))
-BOT.command_prefix_simple = command_prefix
-
-# remove default help command
-BOT.remove_command('help')
-
-# add cogs dynamically
-for cog_name in DiscordBot.Cogs.__all__:
-    cog_module = __import__('DiscordBot.Cogs.%s' % cog_name, fromlist=[cog_name])
-    if hasattr(cog_module, cog_name):  # ignores "work in progress" cogs
-        cog = getattr(cog_module, cog_name)(BOT)
-        BOT.add_cog(cog)
-
-
-@BOT.event
-async def on_ready():
-    logging.info('Logged in as: %s, (id: %s)' % (BOT.user.name, BOT.user.id))
-    logging.info('Channels connected to:')
-    for channel in BOT.get_all_channels():
-        logging.info('    %s.%s (%s) (id: %s)' % (channel.server.name, channel.name, str(channel.type), channel.id))
-    print('Bot is ready')
-    print('------------')
-
-    await BOT.change_presence(game=discord.Game(name='Type %shelp' % command_prefix))
-
-
-@BOT.event
-async def on_command_error(error, context):
-    """
-    This will always be called after an exception is raised on a command.
-    In order to avoid duplicate log entries, errors that should be treated individually per command are ignored here.
-    """
-    # Ignore non-existent and disabled commands
-    # Handle these 6 exceptions for each command individually
-    if isinstance(error, (commands.CommandNotFound, commands.DisabledCommand,
-                          commands.TooManyArguments, commands.MissingRequiredArgument,
-                          commands.BadArgument, commands.CommandOnCooldown,
-                          commands.NoPrivateMessage, commands.CheckFailure)):
-        pass
-    # Exception handled in Math.command_divide
-    elif (isinstance(error, commands.CommandInvokeError) and
-          isinstance(error.original, ZeroDivisionError) and
-          context.command.callback is Math.command_divide.callback):
-        pass
-    elif (isinstance(error, commands.CommandInvokeError) and
-          isinstance(error.original, beckett.exceptions.InvalidStatusCodeError)):
-        pass
-    else:
-        print('Ignoring exception in command', context.invoked_with, file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+from DiscordBot.Bot import Bot
 
 
 if __name__ == '__main__':
-    cli_parser = argparse.ArgumentParser(description='Run Discord Bot')
-    cli_parser.add_argument('token', help='Bot token')
-    cli_parser = cli_parser.parse_args()
+    # command line handling
+    cli = argparse.ArgumentParser(description='Run Discord Bot')
+    cli.add_argument('token', help='Bot token')
+    cli = cli.parse_args()
 
-    BOT.run(cli_parser.token)
+    # logging handling
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    logger = logging.getLogger('DiscordBot.main')
 
-    # https://discordapp.com/developers/applications/me
-
-
-# TODO COMMAND send message with delay
-# TODO TESTS!
+    # run bot
+    logger.info('Starting bot')
+    Bot(prefix='!', intents=None).run(cli.token)
+    logger.info('Shutting down bot complete')
---
+++ DiscordBot/Bot.py
@@ -0,0 +1,84 @@
+#!python3
+# coding: utf-8
+
+
+"""
+Bot class
+"""
+
+
+import logging
+import sys
+import discord
+from discord.ext import commands
+import DiscordBot.Cogs
+
+
+class Bot(commands.Bot):
+    """
+    Main Bot class
+    """
+    def __init__(self, prefix, intents, *args, **kwargs):
+        self.logger = logging.getLogger('DiscordBot.Bot')
+
+        prefix = commands.when_mentioned_or(prefix) if prefix else commands.when_mentioned
+        intents = intents if intents else discord.Intents.default()
+
+        super().__init__(command_prefix=prefix, intents=intents, *args, **kwargs)
+
+        self.command_prefix_simple = prefix
+
+        # remove default help command
+        self.remove_command('help')
+
+        # add cogs dynamically
+        for cog_name in DiscordBot.Cogs.__all__:
+            cog_module = __import__(f'DiscordBot.Cogs.{cog_name}', fromlist=[cog_name])
+            if hasattr(cog_module, cog_name):  # ignores "work in progress" cogs
+                cog = getattr(cog_module, cog_name)(self)
+                self.add_cog(cog)
+
+    async def on_ready(self):
+        """
+        Called when bot finishes preparing data received from Discord
+        """
+        self.logger.info(f'Logged in as: {self.user.name}, (id: {self.user.id})')
+        self.logger.info('Channels connected to:')
+        for channel in self.get_all_channels():
+            self.logger.info(f'    {channel.guild.name}.{channel.name} '
+                             f'({str(channel.type)}) (id: {channel.id})')
+
+        await self.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name=f'for @{self.user.display_name} help'))
+
+        self.logger.info('Bot is ready')
+
+    async def on_error(self, event, *args, **kwargs):
+        """
+        Called when an uncaught/unhandled exception occurs
+        """
+        self.logger.exception(
+            f'When handling event: {event}, with arguments: {args}\n'
+            f'{sys.exc_info()[2]}')
+
+    async def on_command_error(self, ctx, exception):
+        """
+        Called when an exception occurs when executing a command
+        """
+        # ignore non-existent commands
+        if isinstance(exception, commands.CommandNotFound):
+            return
+
+        # original logic but with logging
+        if self.extra_events.get('on_command_error', None):
+            return
+        if hasattr(ctx.command, 'on_error'):
+            return
+        if ctx.cog and commands.Cog._get_overridden_method(ctx.cog.cog_command_error) is not None:
+            return
+
+        self.logger.exception(
+            f'When calling command : {ctx.command}\n'
+            f'{sys.exc_info()[2]}')
