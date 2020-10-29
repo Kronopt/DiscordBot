1ca6ed20a416a4952d133b943ff4a11cd100cf8b
Kronopt <Kronopt@users.noreply.github.com>
2020-10-29 00:46:28 +0000

    feat: remove unneeded converter

--- DiscordBot/Converters.py
+++ DiscordBot/Converters.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-Converters convert the arguments of a command.
+Converters convert the arguments of a command
 """
 
 DICES = ('d4', 'd6', 'd8', 'd10', 'd12', 'd20')
@@ -13,17 +13,6 @@ def dice(argument):
         return argument
     else:
         raise ValueError(str(argument) + ' is not a valid dice')
-
-
-def number(argument):
-    try:
-        value = float(argument)
-    except ValueError:
-        raise ValueError(str(argument) + ' is not a valid number')
-    else:
-        # ex: for 7.0 return 7
-        # ex: for 3.4 return 3.4
-        return int(value) if int(value) == value else value
 
 
 def positive_int(argument):
