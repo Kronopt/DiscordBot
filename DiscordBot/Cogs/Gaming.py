2ee4433cba1fbb339fec489108d12dc6deeac567
Kronopt <Kronopt@users.noreply.github.com>
2021-02-09 00:58:41 +0000

    feat: !gamedeal
    needs testing!

--- DiscordBot/Cogs/Gaming.py
+++ DiscordBot/Cogs/Gaming.py
@@ -10,10 +10,11 @@ Gaming related Commands
 import asyncio
 import collections
 import urllib
-from discord.ext import commands
+import discord
 import pyppeteer
+from discord.ext import commands
 from DiscordBot.BaseCog import Cog
-from DiscordBot.Services import AwesomenautsRank
+from DiscordBot.Services import AwesomenautsRank, ExternalAPIHandler, IsThereAnyDealAPI
 
 
 class NoBrowserError(Exception):
@@ -34,6 +35,34 @@ class Gaming(Cog):
         self.emoji = '🎮'
         self.browser = None
         self.awesomenauts_rank_url = 'https://orikaru.net/nautsrankings#//rank/asc/{}//'
+        self.isthereanydeal_base_url = 'https://api.isthereanydeal.com'
+
+        headers = {'Accept': 'application/json',
+                   'User-Agent': 'DiscordBot (https://github.com/Kronopt/DiscordBot)'}
+        self.isthereanydeal_search_api = ExternalAPIHandler.APICommunicationHandler(
+            api_name='IsThereAnyDeal API, Search endpoint',
+            base_url=f'{self.isthereanydeal_base_url}/v02/search/search/?key='
+                     f'{self.bot.cog_args["isthereanydeal_token"]}&limit=1',
+            headers=headers,
+            json_parser=IsThereAnyDealAPI.SearchEndpoint)
+        self.isthereanydeal_game_info_api = ExternalAPIHandler.APICommunicationHandler(
+            api_name='IsThereAnyDeal API, Get Info About Game endpoint',
+            base_url=f'{self.isthereanydeal_base_url}/v01/game/info/?key='
+                     f'{self.bot.cog_args["isthereanydeal_token"]}',
+            headers=headers,
+            json_parser=IsThereAnyDealAPI.GetInfoAboutGameEndpoint)
+        self.isthereanydeal_game_prices_api = ExternalAPIHandler.APICommunicationHandler(
+            api_name='IsThereAnyDeal API, Get Current Game Prices endpoint',
+            base_url=f'{self.isthereanydeal_base_url}/v01/game/prices/?key='
+                     f'{self.bot.cog_args["isthereanydeal_token"]}&region=eu2&country=PT',
+            headers=headers,
+            json_parser=IsThereAnyDealAPI.GetCurrentPricesEndpoint)
+        self.isthereanydeal_game_historical_price_api = ExternalAPIHandler.APICommunicationHandler(
+            api_name='IsThereAnyDeal API, Get Historical Price endpoint',
+            base_url=f'{self.isthereanydeal_base_url}/v01/game/lowest/?key='
+                     f'{self.bot.cog_args["isthereanydeal_token"]}&region=eu2&country=PT',
+            headers=headers,
+            json_parser=IsThereAnyDealAPI.GetHistoricalLowEndpoint)
 
     async def setup_cog(self):
         # init browser
@@ -45,6 +74,41 @@ class Gaming(Cog):
         # os.system(' '.join(launcher.cmd))
 
         self.browser = await launcher.launch()
+
+    async def create_gamedeal_embed(self, game_info, game_prices, game_historical_low_price):
+        # TODO games available in lots of stores makes description go above embed char limit...
+
+        dlc = ' (DLC)' if game_info.is_dlc else ''
+
+        embed = discord.Embed(colour=self.embed_colour, description='')
+        embed.set_author(name=game_info.title + dlc)
+        embed.set_image(url=game_info.image_url)
+        embed.set_footer(text='IsThereAnyDeal.com ❤',
+                         icon_url='https://d2uym1p5obf9p8.cloudfront.net/images/favicon.png')
+        embed.add_field(name='Historical Low',
+                        value=f'__{game_historical_low_price.price}{game_prices.currency}__ '
+                              f'on {game_historical_low_price.store}',
+                        inline=True)
+        embed.add_field(name='Steam Review',
+                        value=f'**{game_info.steam_review.text}**\n'
+                              f'({game_info.steam_review.positive_reviews_percent}% of '
+                              f'{game_info.steam_review.total_reviews} users)',
+                        inline=True)
+
+        for shop in game_prices.shops:
+            price_current = f'{shop.price_full}{game_prices.currency}'
+
+            if shop.price_percent_discount != 0:
+                price_current = f'~~{price_current}~~'
+                price_cut = f'{shop.price_discounted}{game_prices.currency} ' \
+                            f'(-{shop.price_percent_discount}%) '
+            else:
+                price_cut = ''
+
+            embed.description += f'{price_current} {price_cut}[{shop.name}]({shop.game_url})\n'
+        embed.description += '\n'
+
+        return embed
 
     ##########
     # COMMANDS
@@ -133,6 +197,38 @@ class Gaming(Cog):
 
         await page.close()
 
+    # GAMEDEAL
+    @commands.command(name='gamedeal', ignore_extra=False, aliases=['gameprice'])
+    async def command_gamedeal(self, context, *game_name):
+        """
+        Displays game pricing info
+
+        Info pertains to all stores in which the game is available for purchase
+
+        ex:
+        `<prefix>gamedeal` Assassin's Creed Odyssey
+        `<prefix>gameprice` Cyberpunk 2077
+        """
+        if len(game_name) == 0:  # at least one argument
+            param = collections.namedtuple('param', 'name')
+            raise commands.MissingRequiredArgument(param('game_name'))
+
+        game_name = ' '.join(game_name)
+        game_name_quoted = urllib.parse.quote(game_name)
+
+        game = await self.isthereanydeal_search_api.call_api(f'&q={game_name_quoted}')
+        # TODO check if game exists, is ok, etc
+        game_info = await self.isthereanydeal_game_info_api.call_api(f'&plains={game.plain}')
+        # TODO check if needed fields are ok
+        game_prices = await self.isthereanydeal_game_prices_api.call_api(f'&plains={game.plain}')
+        # TODO check if needed fields are ok
+        game_historical_low_price = await self.isthereanydeal_game_historical_price_api.call_api(
+            f'&plains={game.plain}')
+        # TODO check if needed fields are ok
+
+        embed = await self.create_gamedeal_embed(game_info, game_prices, game_historical_low_price)
+        await context.send(embed=embed)
+
     ################
     # ERROR HANDLING
     ################
@@ -144,7 +240,7 @@ class Gaming(Cog):
             (commands.CommandOnCooldown, commands.NoPrivateMessage, commands.CheckFailure))
 
     @command_awesomenauts_rank.error
-    async def awesomenauts_on_error(self, context, error):
+    async def awesomenauts_rank_on_error(self, context, error):
         bot_message = f'`{context.prefix}{context.command.qualified_name}` expects an ' \
                       'Awesomenauts player name as argument'
         timeout_message = 'Can\'t retrieve Awesomenauts rankings at the moment'
@@ -157,3 +253,15 @@ class Gaming(Cog):
             (commands.MissingRequiredArgument, bot_message),
             (NoBrowserError, timeout_message),
             (pyppeteer.errors.TimeoutError, timeout_message))
+
+    # TODO error handling
+    @command_gamedeal.error
+    async def gamedeal_on_error(self, context, error):
+        bot_message = f'`{context.prefix}{context.command.qualified_name}` error'
+
+        await self.generic_error_handler(
+            context, error,
+            (commands.CommandOnCooldown, commands.NoPrivateMessage, commands.CheckFailure),
+            (commands.TooManyArguments, bot_message),
+            (commands.BadArgument, bot_message),
+            (commands.MissingRequiredArgument, bot_message))
